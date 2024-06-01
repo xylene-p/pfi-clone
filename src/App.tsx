aa589ebacd85e552096022eef5a904887fb3d3a4
Kat Perez <xylene.dev@gmail.com>
2024-06-01 10:08:51 -0400

    change title

--- src/App.tsx
+++ src/App.tsx
@@ -9,31 +9,10 @@ type AppProps = {
 };
 
 const App: FC<AppProps> = ({ title }) => {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>{title}</h1>
+      <head>{title}</head>
       <Post />
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   );
 };
