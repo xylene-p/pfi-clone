6bfe902b936248ad14c1e2687c01aac63416e052
Kat Perez <xylene.dev@gmail.com>
2024-06-01 10:13:50 -0400

    cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,12 @@
-import { useState, FC } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import Post from "./Post";
 
-type AppProps = {
-  title: string;
-};
-
-const App: FC<AppProps> = ({ title }) => {
+function App() {
   return (
     <>
-      <head>{title}</head>
       <Post />
     </>
   );
-};
+}
 
 export default App;
