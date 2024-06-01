531180f16027135aac029c99aa470dfbfd43d146
Kat Perez <xylene.dev@gmail.com>
2024-06-01 10:14:58 -0400

    remove title

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,6 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App title="perfectly imperfect clone" />
+    <App />
   </React.StrictMode>
 );
